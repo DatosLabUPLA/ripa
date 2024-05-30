9891de47f373a18646e80834a44a5396f59cc787
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2024-05-30 19:35:46 -0400

    Add: Datos revista y piblicaciones para nueva colección

---
+++ bq_ripa_v2.sh
@@ -0,0 +1,85 @@
+#! /bin/bash
+
+# Configuración de la cuenta de Google Cloud
+echo "Configurando proyecto"
+gcloud config set project ripa-1022
+
+# Definir variable de tiempo de inicio
+start_time=`date +%s`
+
+# Actualizar repositorio
+echo "Revisando si el proyecto existe y/o actualizandolo"
+git clone https://github.com/DatosLabUPLA/ripa
+cd ripa
+git pull
+cd ~
+
+# Instrucciones previas
+echo "--------------------------"
+echo "INSTRUCCIONES PREVIAS V2"
+echo "1) Antes de continuar, asegurese de haber creado el dataset scholarly en BigQuery"
+echo "2) Asegurese de haber eliminado las tablas pubs, authors y coauthors en el dataset scholarly"
+echo "--------------------------"
+
+# Pregunta si desea actualizar los datos
+echo "Desea actualizar los datos? (y/n)"
+read actualizar
+
+# Si la respuesta es "y" se actualizan los datos
+if [ $actualizar = "y" ]; then
+    echo "Actualizando datos"
+
+    # Scimago 
+    echo "Cargando datos a BigQuery - Scimago"
+    cd ripa/APP\ PROCESA\ DATOS/
+    bq load --source_format=CSV --field_delimiter="tab" --skip_leading_rows=1 scholarly.scimago scimago_journals_categories.csv id_journal:STRING,journal:STRING,journal_short:STRING,id_journal2:STRING,id_area:STRING,area_name:STRING,id_category:STRING,categories:STRING;
+
+    # Tabla pubs_journal
+    echo "Creando tabla de publicaciones con el nombre del pubs_journal"
+    bq query --use_legacy_sql=false \
+    "create or replace TABLE scholarly.pubs_journal as
+    SELECT scholar_id, lower(
+      regexp_replace(
+        replace(
+        replace(
+        replace(
+        replace(
+        replace(
+        replace(
+          replace(
+            replace(
+              replace(
+                replace(
+                  replace(citation,' ',''),',',''
+                ), '(',''
+              ), ')',''
+            ),'-',''
+          ), 'á','a'
+        ),'é', 'e'
+        ),'í','i'
+        ),'ó','o'
+        ),'ú','u'
+        ), 'ñ','n'
+
+          ),r'([0-9]+)',''
+        )
+      ) as short_js
+    from scholarly.Info_Publicaciones
+    "
+
+    # Tabla authors_categories
+    echo "Creando tabla de autores con categorias"
+    bq query --use_legacy_sql=false \
+    "create or replace table scholarly.authors_categories as
+    SELECT p.id_gs, p.short_js,s.journal,s.id_area, s.area_name, s.id_category,s.categories  
+    FROM ripa-1022.scholarly.pubs_journal p, ripa-1022.scholarly.scimago s
+    where p.short_js = s.journal_short"
+else
+    echo "No se actualizarán los datos"
+fi
+
+# Definir variable de tiempo de finalización
+end_time=`date +%s`
+
+# Mostrar tiempo de ejecución
+echo "Tiempo de ejecución: $((end_time-start_time)) segundos"
